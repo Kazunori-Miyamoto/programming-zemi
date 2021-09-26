10f5fc906fb3320a62b6938f0c006f145daa92ea
Kazunori-Miyamoto <190561044@ccmailg.meijo-u.co.jp>
2021-09-26 04:42:23 +0000

    3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # 以下のコードを追加してみましょう
+  get 'home' => "static_pages#home"
+  get 'help' => "static_pages#help"
+  get 'about' => "static_pages#about"
   get 'hello-world' => "foo#hello"
 end
