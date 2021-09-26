c2ac658950890331391b2f9c76426761ee541154
Kazunori-Miyamoto <190561044@ccmailg.meijo-u.co.jp>
2021-09-26 04:19:59 +0000

    simple tweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # 以下のコードを追加してみましょう
   get 'hello-world' => "foo#hello"
