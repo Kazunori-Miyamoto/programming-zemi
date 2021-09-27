6d8d450fa006b0a77ea25565a5aa5923af1567ef
Kazunori-Miyamoto <190561044@ccmailg.meijo-u.ac.jp>
2021-09-27 16:16:29 +0000

    レイアウト完了2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'static_pages/home'
   get 'static_pages/help'
   root 'static_pages#home'
@@ -7,12 +6,7 @@ Rails.application.routes.draw do
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   
-=======
-  root 'static_pages#home'
-  # get 'static_pages/home'
-  # get 'static_pages#help'
-  # root 'static_pages#home'
->>>>>>> layout_with_bootstrap
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # 以下のコードを追加してみましょう
   get 'home' => "static_pages#home"
