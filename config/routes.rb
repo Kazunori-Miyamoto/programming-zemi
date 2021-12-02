f21255bc7ae54bcbbf61dcdf8184c065899647ab
Kazunori-Miyamoto <190561044@ccmailg.meijo-u.ac.jp>
2021-12-02 11:14:41 +0000

    user controller and model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  get  'signup' => "users#new"
   get 'static_pages/home'
   get 'static_pages/help'
   root 'static_pages#home'
   get 'home' => "static_pages#home"
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
-  
+  get  'signup' => "users#new"
+end  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # 以下のコードを追加してみましょう
-  get 'home' => "static_pages#home"
-  get 'help' => "static_pages#help"
-  get 'about' => "static_pages#about"
-  resources :users
-end
+
